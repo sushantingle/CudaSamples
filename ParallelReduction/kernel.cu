5f6d07a0afe7f602736aa1884f291d277c3eda98
Sushant Ingle <sushant.ingle1990@gmail.com>
2021-06-29 08:40:41 +0530

    Reduction technique: - Force addition of neighbours - Interleaved Pair

--- ParallelReduction/kernel.cu
+++ ParallelReduction/kernel.cu
@@ -5,6 +5,62 @@
 #include <stdio.h>
 #include <cstdlib>
 
+// **********************************************************//
+// Neighboured pair reduction has lot of divergence. So, to overcome the divergence, we  will be implementing two approaches.
+// - Force summation of neighbouring approach
+// - Interleaved pair approach
+
+// Elapsed Time : 58.8ms
+__global__ void neighboured_pair_improved(int* data, int* temp, int size)
+{
+    int tid = threadIdx.x;
+    int gid = blockDim.x * blockIdx.x + threadIdx.x;
+
+    int* i_data = data + blockDim.x * blockIdx.x;
+
+    if (gid > size)
+        return;
+
+    for (int offset = 1; offset <= blockDim.x / 2; offset *= 2)
+    {
+        int index = 2 * offset * tid;
+        if (index < blockDim.x)
+        {
+            i_data[index] += i_data[index + offset];
+        }
+        __syncthreads();
+    }
+
+    if (tid == 0)
+    {
+        temp[blockIdx.x] = data[gid];
+    }
+}
+
+// Elapsed Time : 61.1ms
+__global__ void interleaved_pair_reduction(int* data, int* temp, int size)
+{
+    int tid = threadIdx.x;
+    int gid = blockDim.x * blockIdx.x + threadIdx.x;
+
+    int* i_data = data + blockDim.x * blockIdx.x;
+
+    if (gid > size)
+        return;
+
+    for (int offset = blockDim.x / 2; offset > 0; offset /= 2)
+    {
+        i_data[tid] += i_data[tid + offset];
+        __syncthreads();
+    }
+
+    if (tid == 0)
+    {
+        temp[blockIdx.x] = data[gid];
+    }
+}
+// **********************************************************//
+// Elapsed Time : 78.89ms
 __global__ void neighboured_pair_reduction(int* data, int* temp, int size)
 {
     int tId = threadIdx.x;
@@ -74,7 +130,10 @@ int main()
 
     cudaMemset(d_temp, 0, partial_sum_array_size);
     cudaMemcpy(d_input, h_data, byte_size, cudaMemcpyHostToDevice);
-    neighboured_pair_reduction << <grid, block >> > (d_input, d_temp, size);
+
+    // neighboured_pair_reduction << <grid, block >> > (d_input, d_temp, size);
+    //neighboured_pair_improved << <grid, block >> > (d_input, d_temp, size);
+    interleaved_pair_reduction << <grid, block >> > (d_input, d_temp, size);
     cudaDeviceSynchronize();
 
     cudaMemcpy(h_ref, d_temp, partial_sum_array_size, cudaMemcpyDeviceToHost);
