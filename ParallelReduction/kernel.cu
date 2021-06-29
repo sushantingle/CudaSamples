7bbc12c973b40cc21cf5badfa096cc490a8651cb
Sushant Ingle <sushant.ingle1990@gmail.com>
2021-06-29 08:07:17 +0530

    Parallel Reduction Techniques - Neighboured Pair

---
+++ ParallelReduction/kernel.cu
@@ -0,0 +1,100 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <cstdlib>
+
+__global__ void neighboured_pair_reduction(int* data, int* temp, int size)
+{
+    int tId = threadIdx.x;
+    int gId = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (gId > size)
+        return;
+
+    for (int offset = 1; offset <= blockDim.x / 2; offset *= 2)
+    {
+        if (tId % (2 * offset) == 0)
+        {
+            data[gId] += data[gId + offset];
+        }
+        __syncthreads();
+    }
+
+    if (tId == 0)
+    {
+        temp[blockIdx.x] = data[gId];
+    }
+}
+
+
+int Reduction_CPU(int* data, int size)
+{
+    int result = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        result += data[i];
+    }
+    return result;
+}
+
+void Initialize(int* data, int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        data[i] = i;
+    }
+}
+
+int main()
+{
+    int size = 1 << 27; // 128MB size
+    int byte_size = size * sizeof(int);
+    int block_size = 128;
+
+    int* h_data;
+    int* h_ref;
+
+    h_data = (int*)malloc(byte_size);
+    Initialize(h_data, size);
+
+    int CPU_Result = Reduction_CPU(h_data, size);
+
+    dim3 block(block_size);
+    dim3 grid(size / block.x);
+
+    printf("kernel Launch Parameters : Grid %d      Block.x %d \n", grid.x, block.x);
+    int partial_sum_array_size = sizeof(int) * grid.x;
+    h_ref = (int*)malloc(partial_sum_array_size);
+
+    int* d_input, *d_temp;
+    cudaMalloc((void**)&d_input, byte_size);
+    cudaMalloc((void**)&d_temp, partial_sum_array_size);
+
+    cudaMemset(d_temp, 0, partial_sum_array_size);
+    cudaMemcpy(d_input, h_data, byte_size, cudaMemcpyHostToDevice);
+    neighboured_pair_reduction << <grid, block >> > (d_input, d_temp, size);
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(h_ref, d_temp, partial_sum_array_size, cudaMemcpyDeviceToHost);
+
+    int gpu_result = 0;
+    for (int i = 0; i < grid.x; ++i)
+    {
+        gpu_result += h_ref[i];
+    }
+
+    if (CPU_Result == gpu_result)
+    {
+        printf("Results are same on CPU and GPU.\n");
+    }
+
+    cudaFree(d_input);
+    cudaFree(d_temp);
+    free(h_data);
+    free(h_ref);
+
+    cudaDeviceReset();
+    return 0;
+}
--- WarpSample/kernel.cu
+++ WarpSample/kernel.cu
@@ -14,13 +14,69 @@ __global__ void print_warp_details()
 
 }
 
+__global__ void non_divergent_func()
+{
+	int gid = blockIdx.x * blockDim.x + threadIdx.x;
+	float a, b;
+	a = 0.0f;
+	b = 0.0f;
+
+	int warpId = gid / 32;
+	if (warpId % 2 == 0)
+	{
+		a = 100.0f;
+		b = 50.0f;
+	}
+	else
+	{
+		a = 200.0f;
+		b = 200.0f;
+	}
+}
+
+__global__ void divergent_func()
+{
+	int gid = blockIdx.x * blockDim.x + threadIdx.x;
+	float a, b;
+	a = 0.0f;
+	b = 0.0f;
+
+	if (gid % 2 == 0)
+	{
+		a = 100.0f;
+		b = 50.0f;
+	}
+	else
+	{
+		a = 200.0f;
+		b = 200.0f;
+	}
+}
+
 int main()
 {
-	dim3 blocks(42);
-	dim3 grid(2, 2);
+	//***** Warp Details Sample *****//
 
-	print_warp_details << <grid, blocks >> > ();
+	//dim3 blocks(42);
+	//dim3 grid(2, 2);
+
+	//print_warp_details << <grid, blocks >> > ();
+	//cudaDeviceSynchronize();
+	
+	//******************************//
+
+	//***** Warp Divergence Sample *****//
+
+	int size = 1 << 22;
+	dim3 block_size(128);
+	dim3 grid_size((size + block_size.x - 1) / block_size.x);
+
+	non_divergent_func << <grid_size, block_size >> > ();
 	cudaDeviceSynchronize();
 
+	divergent_func << <grid_size, block_size >> > ();
+	cudaDeviceSynchronize();
+
+	//******************************//
 	return 0;
 }
